850ff7c92cd20125876c7f93b450ce4da484ae0b
sumitmaurya2 <sumitkushwaham018@gmail.com>
2026-02-20 13:53:10 +0530

    2.1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <>
       <div className="nav flex items-center justify-between px-[100px] h-[90px] border-b-[1px] border-gray-800">
         <div className="logo">
-         <h3 className='text-[25px] font-[700] sp-text'>GenUI</h3>
+         <h3 className='text-[25px] font-[700] sp-text'>Aedify AI</h3>
         </div>
         <div className="icons flex items-center gap-[15px]">
           <div className="icon"><HiSun /></div>
